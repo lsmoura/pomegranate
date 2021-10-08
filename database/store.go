aefa0eb1d9d7de06947287c0f5f903d3c30f2534
Sergio Moura <sergio@moura.ca>
2021-10-08 10:31:03 -0400

    implement filters (#9)

--- database/store.go
+++ database/store.go
@@ -57,6 +57,62 @@ func (s *store) FindByID(ctx context.Context, dst interface{}, id string) error 
 	return nil
 }
 
+func compare(value interface{}, c Comparison) (bool, error) {
+	typeOfValue := reflect.TypeOf(value)
+	if typeOfValue.Kind() != reflect.TypeOf(c.Value).Kind() {
+		return false, fmt.Errorf("cannot compare different types: %s, %s", typeOfValue.Kind(), reflect.TypeOf(c.Value).Kind())
+	}
+
+	switch typeOfValue.Kind() {
+	case reflect.String:
+		if c.Operator == Equal {
+			return value.(string) == c.Value.(string), nil
+		}
+		return false, fmt.Errorf("comparison not implemented: %s, %d", typeOfValue.Kind(), c.Operator)
+	case reflect.Int32:
+		switch c.Operator {
+		case Equal:
+			return c.Value.(int32) == value.(int32), nil
+		case LessThan:
+			return c.Value.(int32) < value.(int32), nil
+		case LessThanEq:
+			return c.Value.(int32) <= value.(int32), nil
+		case GreaterThan:
+			return c.Value.(int32) > value.(int32), nil
+		case GreaterThanEq:
+			return c.Value.(int32) >= value.(int32), nil
+		}
+		return false, fmt.Errorf("comparison not implemented: %s, %d", typeOfValue.Kind(), c.Operator)
+	}
+
+	return false, fmt.Errorf("comparison not implemented: %s, %d", typeOfValue.Kind(), c.Operator)
+}
+
+func checkFilters(dst interface{}, filters []Filter) (bool, error) {
+	if dst == nil {
+		return false, fmt.Errorf("cannot compare without an object")
+	}
+	if filters == nil {
+		return true, nil
+	}
+
+	valueOf := reflect.ValueOf(dst)
+	for _, filterMap := range filters {
+		for key, comparison := range filterMap {
+			value := valueOf.FieldByName(key)
+			result, err := compare(value.Interface(), comparison)
+			if err != nil {
+				return false, fmt.Errorf("compare: %w", err)
+			}
+			if !result {
+				return false, nil
+			}
+		}
+	}
+
+	return true, nil
+}
+
 func (s *store) FindAll(ctx context.Context, dst interface{}, filters ...Filter) error {
 	if s.db() == nil {
 		return fmt.Errorf("database was not initialized")
@@ -85,11 +141,18 @@ func (s *store) FindAll(ctx context.Context, dst interface{}, filters ...Filter)
 		for k, bytes := c.First(); k != nil; k, bytes = c.Next() {
 			m := reflect.New(myType)
 			err := json.Unmarshal(bytes, m.Interface())
-			// TODO: apply filter
 			if err != nil {
 				return fmt.Errorf("json.Unmarshal: %w", err)
 			}
-			slice = reflect.Append(slice, m.Elem())
+
+			shouldInclude, err := checkFilters(m.Elem().Interface(), filters)
+			if err != nil {
+				return fmt.Errorf("checkFilters: %w", err)
+			}
+
+			if shouldInclude {
+				slice = reflect.Append(slice, m.Elem())
+			}
 		}
 
 		return nil
