547b83a1befe055c0fa765d043ff607d01a76644
Krishna Indani <indanikrishna@gmail.com>
2021-10-09 05:58:23 -0700

    implement logger for themoviedb package (#16)
    * implement logger for themoviedb package
    
    * update error string format
    
    * fix string format for logging

--- themoviedb/helper.go
+++ themoviedb/helper.go
@@ -2,6 +2,19 @@ package themoviedb
 
 type Themoviedb struct {
 	apiKey string
+	Logger Logger
+}
+
+type Logger interface {
+	Log(serviceName string, format string, a ...interface{})
+}
+
+func (d Themoviedb) log(format string, a ...interface{}) {
+	if d.Logger == nil {
+		return
+	}
+
+	d.Logger.Log("themoviedb", format, a...)
 }
 
 func New(apiKey string) Themoviedb {
@@ -11,9 +24,9 @@ func New(apiKey string) Themoviedb {
 }
 
 func (d Themoviedb) ReadSingleMovie(key string) (SingleMovieResponse, error) {
-	return ReadSingleMovie(d.apiKey, key)
+	return ReadSingleMovie(d.apiKey, key, d.log)
 }
 
 func (d Themoviedb) ReadMovies(search string, page int) (Response, error) {
-	return ReadMovies(d.apiKey, search, page)
+	return ReadMovies(d.apiKey, search, page, d.log)
 }
